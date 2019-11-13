a645afa66c1c84062d4d6b432ebb78aacf50cfd9
kush07 <ankushk2518@gmail.com>
2019-11-14 00:13:51 +0530

    Jai Mata Di

--- app.js
+++ app.js
@@ -82,8 +82,10 @@ if (process.env.NODE_ENV === 'production') {
   // app.use((req, res) => {
   //   res.sendFile(path.join(__dirname, 'lmatui', 'build', 'index.html'))
   // })
+  app.use(express.static('lmatui/build'));
+    
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/lmatui/build/index.html'));
+    res.sendFile(path.join(__dirname, 'lmatui', 'build', 'index.html'));
   });
 }
 
